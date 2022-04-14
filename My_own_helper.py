a07d145312ca6e436c8a51771a8cc89816e8562d
Henry Ford <evg@croxsoft.com>
2022-04-14 13:59:56 +0200

    Save files

---
+++ My_own_helper.py
@@ -0,0 +1,53 @@
+string = 'Zhenya'
+integer = 25
+boolean = False
+dictionary = {'name' : 'Dmitriy', 'gender' : 'M'}
+set = {1,2,3,2,2,1,1,2,2,}
+float = 1.25
+tuple = ('Sunday','Monday','Tuesday','Wednesday')
+list = [string,integer,boolean, dictionary, set,float,tuple]
+
+string, integer, boolean = 'Zhenya', 25, False 
+''' Можно писать переменные в одну строку'''
+
+a = dictionary['name']
+b = list[0]
+c = list[3]['gender']
+d = tuple[0]
+print(a)
+print(b)
+print(c)
+print(d)
+abc = list.append('Hello World')
+print(list)
+
+# Перебираем значения в коллекции для аргумента "i". 
+for i in list:
+    if i == 1.25:
+        print('We Found')
+
+for i in range(1,10):
+    print(i)
+
+e = [i*2 for i in range(1,10)]
+print(d)
+
+f = [(tuple[0],tuple[3]) for i in range(5)]
+print(f)
+
+print(f'My name is {a}')
+
+dictionary = {'name' : 'Dmitriy', 'gender' : 'M'}
+a = dictionary['name']
+print(f"My name is {dictionary['name']}")
+
+
+z = list.count(1.25)
+print(z)
+
+
+s = [1,2,4]
+k = iter(s)
+print(next(k))
+print(next(k))
+print(next(k))
