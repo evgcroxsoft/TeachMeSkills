9277f262c0b0f6f10ca0390f1f10dba75b2b16c0
Henry Ford <evg@croxsoft.com>
2022-04-20 14:58:25 +0200

    Finish dz12, created Virtual environment and PIP package

--- dz12.py
+++ dz12.py
@@ -18,5 +18,16 @@ pip unistall
 pip list | grap fastapi
 '''
 # Загрузить пакет на Pypi
+'''
+Статья полезная с описанием действий.
+https://towardsdatascience.com/how-to-upload-your-python-package-to-pypi-de1b363a1b3
+
+Мой пакет залитый на Pypi
+С первого раза не залился, так имя такое уже существовало, пришлось изменять имя в фале setup.py 
+и после заного создавать dist.
+
+pip install Guess-my-number-EVG==0.1
+
+'''
 
 # Почитать про PEP8 и PEP20
