235a97320973da21c3761127957dccdd0c061c4a
Henry Ford <evg@croxsoft.com>
2022-04-20 12:27:11 +0200

    Update dz 12

---
+++ dz12.py
@@ -0,0 +1,22 @@
+# Создать  и активировать  виртуальное окружение
+'''
+python3 -m venv venv
+source venv/bin/activate
+deactivate
+'''
+# Добавить название вашего виртуального окружения venv в  исключения .gitignore
+'''
+Добавилено название .venv/ в .gitignore
+'''
+# Создать пакет Python добавить стороннюю библиотеку
+'''
+Скачал django, fastapi. 
+Использовал команды 
+pip list
+pip install
+pip unistall
+pip list | grap fastapi
+'''
+# Загрузить пакет на Pypi
+
+# Почитать про PEP8 и PEP20
